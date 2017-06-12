beada591b414cedf843943f0a1c24274fa39cae7
Ilya Polishchuk <effolkronium@gmail.com>
2017-06-12 14:24:45 +0300

    Add real number support. Replase std::swap by arguments swap.

--- source/EasyRandom.hpp
+++ source/EasyRandom.hpp
@@ -24,10 +24,21 @@ namespace EasyRandom {
         };
 
         /**
+        * \brief True if type T is applicable by a std::uniform_real_distribution
+        */
+        template<typename T>
+        struct is_uniform_real {
+            static constexpr bool value =
+                   std::is_same<T,       float>::value
+                || std::is_same<T,      double>::value
+                || std::is_same<T, long double>::value;
+        };
+
+        /**
         * \brief Generate a random integer number in a [from, to] range
         * \param from The first limit number of a random range
         * \param to The second limit number of a random range
-        * \return A random number in a [from, to] range
+        * \return A random integer number in a [from, to] range
         * \note Allow both: 'from' <= 'to' and 'from' >= 'to'
         */
         template<typename A, typename B, typename C = typename std::common_type<A, B>::type>
@@ -36,8 +47,26 @@ namespace EasyRandom {
             && is_uniform_int<B>::value
             && std::is_signed<A>::value != std::is_unsigned<B>::value // Prevent conversion from signed to unsigned
             , C>::type get( A from, B to ) {
-            if( from > to ) std::swap( from, to ); // Allow range from higher to lower
-            return std::uniform_int_distribution<C>{ from, to }( engine );
+            if( from < to ) // Allow range from higher to lower
+                return std::uniform_int_distribution<C>{ from, to }( engine );
+            return std::uniform_int_distribution<C>{ to, from }( engine );
+        }
+
+        /**
+        * \brief Generate a random real number in a [from, to] range
+        * \param from The first limit number of a random range
+        * \param to The second limit number of a random range
+        * \return A random real number in a [from, to] range
+        * \note Allow both: 'from' <= 'to' and 'from' >= 'to'
+        */
+        template<typename A, typename B, typename C = typename std::common_type<A, B>::type>
+        static typename std::enable_if<
+               is_uniform_real<A>::value
+            && is_uniform_real<B>::value
+            , C>::type get( A from, B to ) {
+            if( from < to ) // Allow range from higher to lower
+                return std::uniform_real_distribution<C>{ from, to }( engine );
+            return std::uniform_real_distribution<C>{ to, from }( engine );
         }
     private:
         /// The random number engine
