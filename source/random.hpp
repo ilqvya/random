843673b506f253b85413cf88bbad891f43585899
Ilya Polishchuk <effolkronium@gmail.com>
2017-06-27 15:49:24 +0300

    Try to fix warning error

--- source/random.hpp
+++ source/random.hpp
@@ -171,11 +171,12 @@ namespace effolkronium {
         static T get( std::initializer_list<T> init_list ) 
                 noexcept( noexcept( T{ std::declval<T>( ) } ) ) {
             assert( 0u != init_list.size( ) );
-            return *std::next( init_list.begin( ),
-                get<typename std::initializer_list<T>::size_type>(
-                    0u, typename std::iterator_traits<
-                    decltype( init_list.begin( ) )>::difference_type( 
-                init_list.size( ) - 1u ) ) );
+            return *std::next( 
+                init_list.begin( ),
+                typename std::iterator_traits<
+                decltype( init_list.begin( ) )>::difference_type(
+                    get<typename std::initializer_list<T>::size_type>(
+                    0u, init_list.size( ) - 1u ) ) );
         }
     private:
         /// The random number engine
