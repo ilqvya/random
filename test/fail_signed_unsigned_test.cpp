d73008d1d354b97241c81f13e414925374a4f65d
effolkronium <effolkronium@gmail.com>
2017-08-05 17:28:10 +0300

    Clean

--- test/fail_signed_unsigned_test.cpp
+++
@@ -1,7 +0,0 @@
-#include "catch.hpp"
-#include "current_random.hpp"
-
-TEST_CASE( "FAIL_TEST" "Prevent signed unsigned range common arguments" ) {
-    // Must fail
-    Random DOT get<Random_t::common>( -1, 1u );
-}
