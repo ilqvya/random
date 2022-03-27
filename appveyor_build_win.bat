37c25830765e0730ef952cf6536cf6f678ef4585
effolkronium <effolkronium@gmail.com>
2022-03-27 14:44:29 +0300

    Fix build scripts

--- appveyor_build_win.bat
+++ appveyor_build_win.bat
@@ -5,10 +5,9 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2019" ( IF "%PLATFORM%" == 
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2019" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 16 2019" ) ELSE ( SET ARCH=-A x64 ) )
 
 echo CI_DEBUG_GEN_ARCH: %GEN% %ARCH%
+echo CI_DEBUG_CONFIGURATION: %CONFIGURATION%
 
 cmake . -G %GEN% %ARCH% -DCMAKE_CXX_FLAGS="%additional_flags%"
-
-echo CI_DEBUG_CONFIGURATION: %CONFIGURATION%
 
 cmake --build . --config %CONFIGURATION%
 ctest -C %CONFIGURATION%
