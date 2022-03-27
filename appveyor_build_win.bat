faf430075feb5ccb9e0b74a7a1901e2fdebcb4ef
effolkronium <effolkronium@gmail.com>
2022-03-27 15:07:51 +0300

    Update win ci build script for vs2022

--- appveyor_build_win.bat
+++ appveyor_build_win.bat
@@ -3,10 +3,11 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2017" ( IF "%PLATFORM%" == 
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2013" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 12 2013" ) ELSE ( SET GEN="Visual Studio 12 2013 Win64" ) )
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2019" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 16 2019" ) ELSE ( SET GEN="Visual Studio 16 2019" ) )
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2019" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 16 2019" ) ELSE ( SET ARCH=-A x64 ) )
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 17 2022" ) ELSE ( SET GEN="Visual Studio 17 2022" ) )
+IF "%APPVEYOR_BUILD_WORKER_IMAGE%" == "Visual Studio 2022" ( IF "%PLATFORM%" == "x86" ( SET GEN="Visual Studio 17 2022" ) ELSE ( SET ARCH=-A x64 ) )
 
 echo CI_DEBUG_GEN_ARCH: %GEN% %ARCH%
 echo CI_DEBUG_CONFIGURATION: %CONFIGURATION%
-
 cmake . -G %GEN% %ARCH% -DCMAKE_CXX_FLAGS="%additional_flags%"
 
 cmake --build . --config %CONFIGURATION%
