99bdeec1afe2eae1bc327d5b0644d775881e5136
DolphyWind <yunusaydin590@gmail.com>
2023-03-11 15:59:54 +0300

    Update License year to 2023

--- include/effolkronium/random.hpp
+++ include/effolkronium/random.hpp
@@ -7,7 +7,7 @@ ______  ___   _   _______ ________  __
 \_| \_\_| |_/\_| \_/___/  \___/\_|  |_/ https://github.com/effolkronium/random
 
 Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-Copyright (c) 2017-2022 effolkronium (Illia Polishchuk)
+Copyright (c) 2017-2023 effolkronium (Illia Polishchuk)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files( the "Software" ), to deal
