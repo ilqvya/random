98dc7efddd23efc43cc7804a0e48115ef318dbeb
Illia Polishchuk <effolkronium@gmail.com>
2023-07-05 11:40:07 +0200

    Fix compile error with latest libc++ on MacOS
    static_assert(__libcpp_random_is_valid_urng<_URNG>::value, "");

--- test/catch.hpp
+++ test/catch.hpp
@@ -7118,7 +7118,7 @@ namespace Catch {
 namespace Catch {
 
     struct RandomNumberGenerator {
-        typedef std::ptrdiff_t result_type;
+        typedef std::uint64_t result_type;
 
         result_type operator()( result_type n ) const { return std::rand() % n; }
 
